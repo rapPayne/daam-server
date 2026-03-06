d77fb72380578d3835c0148bbf8d11678a48e10b
rapPayne <bob@creator.net>
2026-03-06 12:25:21 -0600

    Removed X-Powered-By

--- middlewares/logging-middleware.mjs
+++ middlewares/logging-middleware.mjs
@@ -1,6 +1,5 @@
 
 export const loggingMiddleware = (req, res, next) => {
-  res.set('X-Powered-By', 'Agile Gadgets');
   console.log('*'.repeat(80))
   console.log(`Request: ${req.method} ${req.url}`)
   next();
